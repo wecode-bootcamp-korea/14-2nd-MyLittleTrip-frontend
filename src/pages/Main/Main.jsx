c51d0152ea95b0cddbce6c71b4e3081cc402a133
mengkki <guswnl0610@naver.com>
2020-11-30 19:32:17 +0900

    [add] navbar 레이아웃

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Navtrans from "../../components/Navtrans/Navtrans";
+import Navtrans from "../../Components/Navtrans/Navtrans";
 
 class Main extends Component {
   render() {
