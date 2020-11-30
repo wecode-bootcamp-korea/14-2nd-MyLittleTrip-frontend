06d914c37733c553b8342eb0cd179b29d5f15760
mengkki <guswnl0610@naver.com>
2020-11-30 15:54:57 +0900

    [add] CRA 초기세팅

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Main} from './pages/Main/Main';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact to="/" component={Main}/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/pages/Booking/Booking.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Booking extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Booking;
