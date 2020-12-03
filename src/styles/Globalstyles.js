06048516ffe707319c29b0f2c8ec4bc9246f406e
mengkki <guswnl0610@naver.com>
2020-12-03 15:31:43 +0900

    [add] 카카오 소셜 로그인 기능 추가

--- src/styles/Globalstyles.js
+++ src/styles/Globalstyles.js
@@ -2,17 +2,23 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 const GlobalStyles = createGlobalStyle`
   ${reset}
-
   *{
     box-sizing: border-box;
+    outline: none;
   }
 
   input{
     border-style: none;
+    cursor: pointer;
+  }
+  
+  button{
+    border-style: none;
+    cursor: pointer;
   }
 
   a{
     text-decoration: none;
   }
 `;
-export default GlobalStyles;
+export default GlobalStyles; 
