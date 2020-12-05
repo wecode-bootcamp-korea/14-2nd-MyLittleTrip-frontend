cf8f187536246f4bdb5a2ff140e57853c9d8594d
yoonl <kyouk12a@gmail.com>
2020-12-05 13:31:38 +0900

    [add] 리뷰반영 리팩토링

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './Routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+import "./styles/reset.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
